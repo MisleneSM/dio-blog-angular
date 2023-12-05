5872eb346d67ada5a6c508ab0b3f8200553e4015
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-12-05 20:47:57 -0300

    Ajustado API novamente

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,16 +2,16 @@ export const dataFake = [
     {
        "id": '1',
        "photo": "https://2.bp.blogspot.com/-bWmBhSmhHXc/VbuNzVvK5nI/AAAAAAAAOn4/6-wtd_i-txQ/s1600/capa.jpg",
-       "photo2": '../..//src/assets/mario.png',
-       "photo3": "../..//src/assets/luigi.png",
+       "photo2": '../../../assets/mario.png',
+       "photo3": "../../../assets/luigi.png",
        "title": "SUPER MARIO BROS",
        "description": "Super Mario Bros é um jogo de plataforma icônico que segue as aventuras do encanador Mario, que embarca em uma missão para resgatar a Princesa Peach do malvado vilão King Koopa (também conhecido como Bowser). O jogo se passa no fictício Reino dos Cogumelos, e os jogadores percorrem vários mundos, enfrentando obstáculos, inimigos e coletando power-ups ao longo do caminho",
     },
     {
         "id": '2',
         "photo": "https://www.arrobanerd.com.br/wp-content/uploads/2023/06/maxresdefault-27.jpg",
-        "photo2": 'src/assets/sonic-left.png',
-        "photo3": "../..//src/assets/sonic-rigth.png",
+        "photo2": '../../../assets/sonic-left.png',
+        "photo3": "../../../assets/sonic-rigth.png",
         "title": "SONIC SUPERSTARS",
         "description": `A Nintendo e a SEGA anunciaram na Nintendo Direct a chegada de Sonic Superstars para Nintendo Switch, ainda em 2023. Junte-se a Sonic, Tails, Knucles e Amy nessa nova aventura.
         Aventure-se pelas místicas Ilhas Northstar neste clássico jogo de plataforma de ação em alta velocidade do Sonic.`
@@ -19,16 +19,16 @@ export const dataFake = [
      {
         "id": '3',
         "photo": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/pt_BR/games/switch/c/crash-bandicoot-n-sane-trilogy-switch/hero",
-        "photo2": "../..//src/assets/crash-left.png",
-        "photo3": "../..//src/assets/crash-rigth.png",
+        "photo2": "../../../assets/crash-left.png",
+        "photo3": "../../../assets/crash-rigth.png",
         "title": "CRASH BANDICOOT",
         "description": `Crash Bandicoot é a primeira parcela da série de 16 títulos. Embora o conjunto de jogos apresente vários gêneros: corridas, beat'em'ups e plataformas, a primeira parte concentra-se no estilo de plataforma. O jogo é desenvolvido pela Naughty Dog, atualmente famosa por fazer as séries Jax and Daxter, Uncharted e The Last of Us. O jogo apresenta o protagonista bandicoot autointitulado tentando superar os obstáculos enquanto derruba os inimigos, pulando e circulando-os e coletando frutas e máscaras que lhe dão vários bônus.`,
      },
      {
         "id": '4',
         "photo": "https://img.youtube.com/vi/bXHDSA35_2k/maxresdefault.jpg",
-        "photo2": "../..//src/assets/donkey-left.png",
-        "photo3": "../..//src/assets/donkey-rigth.png",
+        "photo2": "../../../assets/donkey-left.png",
+        "photo3": "../../../assets/donkey-rigth.png",
         "title": "DONKEY KONG COUNTRY",
         "description": `Donkey Kong Country é um jogo de plataforma em 2D desenvolvido pela Rare e publicado pela Nintendo para o Super Nintendo Entertainment System (SNES). Estrelando o personagem Donkey Kong, o jogo se passa em "Donkey Kong Island" onde é explorado seis regiões com ambientes diferentes, e o enredo gira em torno de Donkey Kong e seu sobrinho Diddy Kong, que devem recuperar seu tesouro de bananas roubado por King K. Rool e os Kremlings.`,
      },
