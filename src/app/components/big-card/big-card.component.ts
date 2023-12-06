538752a87ce3319e81ed72d73082ca22af0c1579
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-12-05 22:20:35 -0300

    Ajustado scroll barra horizontal

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,11 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input() id:string = '0'
   @Input() photoCover:string =''
   @Input() cardTitle:string = ''
-
   @Input() cardDescription:string = ''
-  @Input() id:string = '0'
 
   constructor() { }
 
